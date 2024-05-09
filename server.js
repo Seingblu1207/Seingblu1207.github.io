013ed60325ed141d8c65d0384b6b42303658605f
Shaamelo <samuel1221@hotmail.es>
2024-05-09 13:26:08 -0500

    Arreglando bugs

--- server.js
+++ server.js
@@ -21,34 +21,34 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 app.post('/register', async (req, res) => {
-    console.log('Solicitud de registro recibida:', req.body);
-    const { name, email, password } = req.body;
-    const user = new User({ name, email, password });
-    try {
-      await user.save();
-      res.status(201).send();
-    } catch (err) {
-      console.error('Error al registrar usuario:', err);
-      res.status(500).send(); // 500 es el código de estado para "Error interno del servidor"
-    }
-  });
-  
-app.post('/login', async (req, res) => {
-    console.log('Solicitud de inicio de sesión recibida:', req.body);
-    const { email, password } = req.body;
-    try {
-      const user = await User.findOne({ email, password });
-      if (!user) return res.status(400).send();
-      res.status(200).send();
-    } catch (err) {
-      console.error('Error al iniciar sesión:', err);
-      res.status(500).send();
-    }
-  });
+  console.log('Solicitud de registro recibida:', req.body);
+  const { name, email, password } = req.body;
+  const user = new User({ name, email, password });
+  try {
+    await user.save();
+    res.status(201).send();
+  } catch (err) {
+    console.error('Error al registrar usuario:', err);
+    res.status(500).send(); // 500 es el código de estado para "Error interno del servidor"
+  }
+});
 
-app.listen(3000, () => console.log('Server started'));
+app.post('/login', async (req, res) => {
+  console.log('Solicitud de inicio de sesión recibida:', req.body);
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email, password });
+    if (!user) return res.status(400).send();
+    res.status(200).send();
+  } catch (err) {
+    console.error('Error al iniciar sesión:', err);
+    res.status(500).send();
+  }
+});
+
+app.listen(4000, () => console.log('Server started'));
 
 app.get('/users', async (req, res) => {
-    const users = await User.find();
-    res.send(users);
-  });
+  const users = await User.find();
+  res.send(users);
+});
